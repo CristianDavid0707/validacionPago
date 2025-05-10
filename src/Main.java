cac2bfd8abf31a1671f7bcc201471202ddf872c5
CristianDavid0707 <cristiandavipatino679@gmail.com>
2025-05-10 11:49:38 -0500

    Se realiza la logica de programacion para darle solucion a el ejercicio validacion pago taller de condicional compuesto

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         double montoPagado = 0;
+        System.out.print("Ingrese el monto Pagado: ");
+        montoPagado = sc.nextDouble();
 
-
+        if (montoPagado >= 50000) {
+            System.out.println("Pago Completo.");
+        } else {
+            System.out.println("Pago Insuficiente.");
+        }
     }
 }
